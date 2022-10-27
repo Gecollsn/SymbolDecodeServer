a29b9e3663467ef83e7c13387711b80f1dea26e0
Gecollsn <gecollsn@gmail.com>
2022-10-28 07:23:24 +0800

    define netdl struct

---
+++ src/tools/netdl.go
@@ -0,0 +1,18 @@
+package tools
+
+type DLProgress interface {
+	OnStart()
+	OnProcess(progress uint8)
+	OnComplete()
+	OnFailure(err error)
+}
+
+type INetdl interface {
+	HttpDL(url string, dlpath string, callback DLProgress)
+}
+
+type Netdl struct{}
+
+func (ndl *Netdl) HttpDL(url string, dlpath string, callback DLProgress) {
+
+}
