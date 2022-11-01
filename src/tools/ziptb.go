f410445df947a9e10e7eff0a9b7e4cb21007780b
Gecollsn <gecollsn@gmail.com>
2022-11-02 06:38:35 +0800

    Update ziptb.go

---
+++ src/tools/ziptb.go
@@ -0,0 +1,26 @@
+package tools
+
+type IZipToolbox interface {
+	Zip(folder string, ozip string)
+	Unzip(zfile string, ofolder string)
+	List(zfile string)
+	Extract(zfile string, depth int, name string, ofolder string)
+}
+
+type ZipToolbox struct{}
+
+func (ZipToolbox) Zip(folder string, ozip string) {
+
+}
+
+func (ZipToolbox) Unzip(zfile string, ofolder string) {
+
+}
+
+func (ZipToolbox) List(zfile string) {
+
+}
+
+func (ZipToolbox) Extract(zfile string, depth int, name string, ofolder string) {
+
+}
